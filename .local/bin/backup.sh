d75894900e601a3e3264d28e941e168508654836
Ken Rendell L. Caoile <kaoile.cenrendell@gmail.com>
2023-07-29 01:11:02 +0800

    new file:   ../../.backup 	modified:   ../../.config/newsboat/urls 	new file:   backup.sh

---
+++ .local/bin/backup.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# Backup script for Home Files
+
+[ -d "${1}" ] || { printf 'Usage: backup.sh <repo>\n' 1>&2; exit 1; }
+
+# Get the password for the repository.
+RESTIC_PASSWORD="$(keepassxc-cli show --attributes Password "${HOME}/.secrets/passwords.kdbx" 'Backup/Personal')"
+
+export RESTIC_REPOSITORY="${1}" RESTIC_PASSWORD
+
+# Check if a repository is already initialized.
+if ! restic cat config >/dev/null 2>&1; then
+	printf 'Initialize the repository (y/N)? '; read -r ans;
+	[ -n "${ans}" ] && { [ -z "${ans#y}" ] || [ -z "${ans#Y}" ]; } && restic init;
+	exit 1;
+fi
+
+# Create a backup of home files.
+restic backup --files-from-verbatim="${HOME}/.backup"
+restic check # Check the repository for errors.
