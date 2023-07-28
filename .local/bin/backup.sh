e19df241f07cd4e4645723713a380901b3f26cab
Ken Rendell L. Caoile <kaoile.cenrendell@gmail.com>
2023-07-29 01:26:03 +0800

    modified:   backup.sh 	new file:   restore.sh

--- .local/bin/backup.sh
+++ .local/bin/backup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Backup script for Home Files
+# Backup Home Files
 
 [ -d "${1}" ] || { printf 'Usage: backup.sh <repo>\n' 1>&2; exit 1; }
 
@@ -10,9 +10,9 @@ export RESTIC_REPOSITORY="${1}" RESTIC_PASSWORD
 
 # Check if a repository is already initialized.
 if ! restic cat config >/dev/null 2>&1; then
-	printf 'Initialize the repository (y/N)? '; read -r ans;
-	[ -n "${ans}" ] && { [ -z "${ans#y}" ] || [ -z "${ans#Y}" ]; } && restic init;
-	exit 1;
+	printf 'Initialize the repository (y/N)? '; read -r ans
+	[ -n "${ans}" ] && { [ -z "${ans#y}" ] || [ -z "${ans#Y}" ]; } && restic init
+	exit 1
 fi
 
 # Create a backup of home files.
---
+++ .local/bin/restore.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Restore Home Files from Backup Repository
+
+[ -d "${1}" ] || { printf 'Usage: restore.sh <repo>\n' 1>&2; exit 1; }
+
+# Get the password for the repository.
+RESTIC_PASSWORD="$(keepassxc-cli show --attributes Password "${HOME}/.secrets/passwords.kdbx" 'Backup/Personal')"
+
+export RESTIC_REPOSITORY="${1}" RESTIC_PASSWORD
+
+# Check if a repository is already initialized.
+restic cat config >/dev/null 2>&1 || { printf 'Backup repository is not initialized!\n' 1>&2; exit 1; }
+
+# Restore home files from backup repository.
+restic restore --verify --target="${HOME}/" 'latest'
