d891aeffa9be1cfdb525beffc4564e817055d326
Ken Rendell L. Caoile <kaoile.cenrendell@gmail.com>
2023-07-31 13:55:35 +0800

    modified:   .backup 	modified:   .local/bin/backup.sh 	modified:   .local/bin/restore.sh

--- .local/bin/backup.sh
+++ .local/bin/backup.sh
@@ -4,16 +4,14 @@
 [ -d "${1}" ] || { printf 'Usage: backup.sh <repo>\n' 1>&2; exit 1; }
 
 # Get the password for the repository.
-RESTIC_PASSWORD="$(keepassxc-cli show --attributes Password "${HOME}/.secrets/passwords.kdbx" 'Backup/Personal')" || exit 1
+printf 'Password: '
+RESTIC_PASSWORD="$(stty -echo; head -n 1; stty echo)"
+printf '\n'
 
 export RESTIC_REPOSITORY="${1}" RESTIC_PASSWORD
 
-# Check if a repository is already initialized.
-if ! restic cat config >/dev/null 2>&1; then
-	printf 'Initialize the repository (y/N)? '; read -r ans
-	[ -n "${ans}" ] && { [ -z "${ans#y}" ] || [ -z "${ans#Y}" ]; } && restic init
-	exit 1
-fi
+# Check if a repository is already initialized or the password is correct.
+restic cat config >/dev/null 2>&1 || { printf 'Invalid repository or password!\n' 1>&2; exit 1; }
 
 # Create a backup of home files.
 restic backup --files-from-verbatim="${HOME}/.backup"
--- .local/bin/restore.sh
+++ .local/bin/restore.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 # Restore Home Files from Backup Repository
 
-[ -d "${1}" ] || { printf 'Usage: restore.sh <repo>\n' 1>&2; exit 1; }
+[ -d "${1}" ] || { printf 'Usage: restore.sh <repo> [snapshot-ID]\n' 1>&2; exit 1; }
 
 # Get the password for the repository.
-RESTIC_PASSWORD="$(keepassxc-cli show --attributes Password "${HOME}/.secrets/passwords.kdbx" 'Backup/Personal')" || exit 1
+printf 'Password: '
+RESTIC_PASSWORD="$(stty -echo; head -n 1; stty echo)"
+printf '\n'
 
 export RESTIC_REPOSITORY="${1}" RESTIC_PASSWORD
 
-# Check if a repository is already initialized.
-restic cat config >/dev/null 2>&1 || { printf 'Backup repository is not initialized!\n' 1>&2; exit 1; }
+# Check if a repository is already initialized or the password is correct.
+restic cat config >/dev/null 2>&1 || { printf 'Invalid repository or password!\n' 1>&2; exit 1; }
 
 # Restore home files from backup repository.
-restic restore --verify --target="${HOME}/" 'latest'
+restic restore --verify --target="${HOME}/" "${2:-latest}"
